e1abbb243c67c8717b0540d1170cee54977c3ab9
arul0318 <arulkumarn0318@gmail.com>
2025-05-03 16:50:37 +0530

    ds

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,59 +3,32 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
+use CodeIgniter\Database\Database;
 
-/**
- * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
- */
 abstract class BaseController extends Controller
 {
-    /**
-     * Instance of the main Request object.
-     *
-     * @var CLIRequest|IncomingRequest
-     */
     protected $request;
-
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
-     * @var list<string>
-     */
     protected $helpers = [];
+    protected $db;
+    protected $users;
 
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
-
-    /**
-     * @return void
-     */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
-        $this->db=\Config\Database::connect();
-        $this->users=$this->db->table('users'); 
+        // Establishing the database connection
+        $this->db = \Config\Database::connect();
+        
+        // Make sure the connection is valid
+        if (!$this->db->connID) {
+            throw new \RuntimeException('Unable to connect to the database.');
+        }
 
-        // Preload any models, libraries, etc, here.
-
-        // E.g.: $this->session = service('session');
+        // Access the 'users' table dynamically via the DB connection
+        $this->users = $this->db->table('users');
     }
 }
